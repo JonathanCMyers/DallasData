84fb75ded6da865a9656fe8643711106e16f23ce
Jonathan Myers <Jonathan@achnic.tk>
2015-08-14 13:43:09 -0500

    Update server.R
    Added handling options for splitting by gender and ethnicity, there are still issues where less than the maximum number of data points are being plotted when creating the rChart scatterplot. Means are being listed but they are being output as NaN values, and cannot reach ui.R for some reason.

--- server.R
+++ server.R
@@ -5,11 +5,11 @@ library(xlsx)
 library(quantmod)
 
 options(
-  RCHARD_LIB = "dimple",
-  rcharts.mode="iframesrc",
-  rcharts.cdn=TRUE,
-  RCHART_WIDTH=600,
-  RCHART_HEIGHT=400
+    RCHARD_LIB = "polycharts",
+    rcharts.mode="iframesrc",
+    rcharts.cdn=TRUE,
+    RCHART_WIDTH=600,
+    RCHART_HEIGHT=400
 )
 
 library(knitr)
@@ -23,64 +23,138 @@ dallasData <- as.data.frame(dallasData)
 dallasData$Gender <- as.factor(dallasData$Gender)
 dallasData$Ethnicity <- as.factor(dallasData$Ethnicity)
 
+# Set up data frames segmented by ethnicity
+dallasAmericanIndian <- dallasData[dallasData$Ethnicity == "American Indian",]
+dallasAsian <- dallasData[dallasData$Ethnicity == "Asian",]
+dallasBlack <- dallasData[dallasData$Ethnicity == "Black",]
+dallasHispanic <- dallasData[dallasData$Ethnicity == "Hispanic",]
+dallasOtherAsian <- dallasData[dallasData$Ethnicity == "Other Asian",]
+dallasPacificIslander <- dallasData[dallasData$Ethnicity == "Pacific Islander",]
+dallasWhite <- dallasData[dallasData$Ethnicity == "White",]
+
+ethnicityList <- list("American Indian" = dallasAmericanIndian, "Asian" = dallasAsian, 
+                      "Black" = dallasBlack, "Hispanic" = dallasHispanic, "Other Asian" = dallasOtherAsian,
+                      "Pacific Islander" = dallasPacificIslander, "White" = dallasWhite)
+subData <- dallasData
+outputText <- ""
 
 shinyServer(
-  function(input, output){
-    
-    #getSelectedWords <- reactive({
-    #  if(length(input$word) < 1) { return(NULL) }
-    #  else { return(as.character(input$word)) }
-    #})
-    
-    #output$selectedWords <- renderText({
-    #  text <- getSelectedWords()
-    #  if(length(text) == 0) { return("Nothing selected") }
-    #  else { return(text) }
-    #})
-    
-    #output$triggerRChart <- renderUI({
-    #  if(length(input$word) > 0) { 
-    #   
-    #  }
-    #})
-    
-    #output$ethOptions <- renderUI({
-    #  selectInput(inputId="Ethnicity",
-    #              label="Select a list of ethnicities:",
-    #              choices=c("American Indian", "Asian", "Black", "Hispanic",
-    #                        "Other Asian", "Pacific Islander", "White"),
-    #              multiple=TRUE
-    #  )
-    #})
-    
-    
-    output$chart1 <- renderChart2({
-      
-      d1 <- dPlot(y="Rate_of_Pay", x="Ethnicity", data=dallasData, groups="variable", type="bar")
-      d1$yAxis(type="addCategoryAxis", orderRule="Pay")
-      d1$xAxis(type="addPctAxis")
-      return(d1)
-      
-      #p1 <- nPlot(Rate_of_Pay ~ Ethnicity, data=dallasData, group="Gender", type="scatterChart")
-      #p1$xAxis(axisLabel = "Ethnicity")
-      #p1
-      
-      #p4 <- nPlot( ~ cyl, data=mtcars, type="pieChart")
-      #return(p4)
-      
-      #subData <- dallasData[dallasData$Ethnicity=="White",]
-      
-      #r1 <- rPlot(Rate_of_Pay ~ Ethnicity, data=subData, color="Gender", type="point")
-      #r1 <- rPlot(x = "Ethnicity", y = "Rate_of_Pay", data=subData, type="box")
-      #r1$addParams(height=600, width=800, dom="chart1", title="Rate of Pay by Ethnicity")
-      #r1$set(fontSize="50px")
-      #return(r1)
-      #dmap <- dplot(x=c(""))
-    })
-    
-    
-    #output$oid1 <- renderPrint({input$id1})
-    #output$oid2 <- renderPrint({input$id2})
-    #output$odate <- renderPrint({input$date})
-  }    
+    function(input, output){
+        
+        #getSelectedWords <- reactive({
+        #  if(length(input$word) < 1) { return(NULL) }
+        #  else { return(as.character(input$word)) }
+        #})
+        
+        #output$selectedWords <- renderText({
+        #  text <- getSelectedWords()
+        #  if(length(text) == 0) { return("Nothing selected") }
+        #  else { return(text) }
+        #})
+        
+        #output$triggerRChart <- renderUI({
+        #  if(length(input$word) > 0) { 
+        #   
+        #  }
+        #})
+        
+        #output$ethOptions <- renderUI({
+        #  selectInput(inputId="Ethnicity",
+        #              label="Select a list of ethnicities:",
+        #              choices=c("American Indian", "Asian", "Black", "Hispanic",
+        #                        "Other Asian", "Pacific Islander", "White"),
+        #              multiple=TRUE
+        #  )
+        #})
+        #output$testVector <- renderText({
+        #    if(length(input$eth2) > 0) { return(input$eth2) }
+        #    else { return("-1") }
+        #})
+        #output$lengthOfSubData <- renderText({
+        #    return(nrow(subData))
+        #})
+        output$chart1 <- renderChart2({
+            ## Combine the ethnicities chosen by the user into one data frame, which is plotted
+            if(length(input$eth2) == 0) {
+                subData <- data.frame("X."=numeric(0), "Gender"=character(0), "Ethnicity"=character(0), 
+                                      "Adjusted_Hire_Date"=numeric(0), "Rate_of_Pay"=numeric(0), "Annual_Hours"=numeric(0))
+            }
+            else if(length(input$eth2) == 1) {
+                subData <- ethnicityList[[input$eth2]]
+            }
+            else if(length(input$eth2) == 2) {
+                subData <- rbind(ethnicityList[[input$eth2[1]]], ethnicityList[[input$eth2[2]]])
+            }
+            else if(length(input$eth2) > 2) {
+                count <- 1
+                subData <- data.frame("X."=numeric(0), "Gender"=character(0), "Ethnicity"=character(0), 
+                                      "Adjusted_Hire_Date"=numeric(0), "Rate_of_Pay"=numeric(0), "Annual_Hours"=numeric(0))
+                subData$Gender <- as.factor(subData$Gender)
+                subData$Ethnicity <- as.factor(subData$Ethnicity)
+                repeat{
+                    subData <- rbind(subData, ethnicityList[[input$eth2[count]]])
+                    if(count == length(input$eth2)) { break }
+                    count <- count+1
+                }
+            }
+            # The entire decision tree above does not have any difference from:
+                    #subData <- dallasData[dallasData$Ethnicity %in% input$eth2,]
+            # in regards to how many points are plotted by rCharts
+            
+            
+            if(input$graphMeans == FALSE) {
+                ## Shows different graphs depending on if we are dividing the output by gender or not
+                if(input$divideByGender == TRUE) {
+                    r1 <- rPlot(Rate_of_Pay ~ Ethnicity, data=subData, color="Gender", type="point")
+                }
+                else {
+                    r1 <- rPlot(Rate_of_Pay ~ Ethnicity, data=subData, color="Ethnicity", type="point")
+                }
+                r1$addParams(height=600, width=800, dom="chart1", title="Rate of Pay by Ethnicity")
+                r1$set(fontSize="50px")
+            }
+            else if(input$graphMeans == TRUE) {
+                if(input$divideByGender == TRUE) {
+                    r1 <- rPlot(mean(Rate_of_Pay) ~ Ethnicity | Gender, data=subData, color="Gender", type="bar")
+                    #payMeans <- numeric(length(input$eth2)*2)
+                    outputText <- ""
+                    for(i in 1:(length(input$eth2))){
+                        outputText <- cat(outputText, "Mean Rate of Pay for", input$eth2[i], "males: $")
+                        
+                        tempFrame <- subData[subData$Ethnicity == input$eth2[i],]
+                        tempFrameM <- tempFrame[tempFrame$Gender == "Male",5]
+                        cat(outputText, mean(tempFrameM))
+                        
+                        tempFrameF <- tempFrame[tempFrame$Gender == "Female",5]
+                        outputText <<- cat(outputText, "     females: $", mean(tempFrameF), "\n")
+                    }
+                    #output$meanInformation <- outputText
+                }
+                else {
+                    r1 <- rPlot(mean(Rate_of_Pay) ~ Ethnicity, data=subData, color="Ethnicity", type="bar")
+                    outputText <- ""
+                    for(i in 1:(length(input$eth2))){
+                        outputText <- cat(outputText, "Mean Rate of Pay for", input$eth2[i], "s: $")
+                        
+                        tempFrame <- subData[subData$Ethnicity == input$eth2[i],]
+                        outputText <<- cat(outputText, colMeans(tempFrame)[["Rate_of_Pay"]], "\n")
+                    }
+                    #output$meanInformation <- outputText
+                }
+                r1$addParams(height=600, width=800, dom="chart1", title="Mean Rate of Pay by Ethnicity")
+            }
+            return(r1)
+        })
+        
+        output$meanInformation <- renderText({
+            return(outputText)
+        })
+        
+        #output$meanInformation <- renderText
+        
+        
+        #output$oid1 <- renderPrint({input$id1})
+        #output$oid2 <- renderPrint({input$id2})
+        #output$odate <- renderPrint({input$date})
+    }    
 )
